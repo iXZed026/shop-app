b4d54a2d8fc3a29665e948b55503efac58fec2e4
iXZed026 <danyal.titanka24@gmail.com>
2024-08-20 17:12:44 +0330

    new product jsx completed

--- src/components/Home/NewPeoducts/NewProduct.js
+++ src/components/Home/NewPeoducts/NewProduct.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const NewProduct = () => {
+const NewProduct = (props) => {
+
+    const { pImage, pName, basePrice } = props;
+
     return (
-        <div>newProduct</div>
+        <div className="new-product">
+            <div className="new-product-contianer">
+                <div className="new-product-flex">
+                    <div className='.new-product-image'>
+                        <img src={pImage} alt="" />
+                    </div>
+                    <h4>{pName}</h4>
+                    <span>{basePrice}</span>
+                </div>
+            </div>
+        </div>
     )
 }
 
