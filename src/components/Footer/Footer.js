7d120530571e25c66f3a549455408b924a8cc07f
iXZed026 <danyal.titanka24@gmail.com>
2024-08-21 15:47:51 +0330

    footer Heading compelted

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,12 +3,13 @@ import "./Footer.css";
 import FooterHeading from './FooterHeading';
 
 
+
 const Footer = () => {
     return (
         <footer>
             <div className="footer-container">
                 <div className="footer-flex">
-                    
+                    <FooterHeading />
                 </div>
             </div>
         </footer>
