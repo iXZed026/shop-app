229cb4f69f7243e0cef837b8d43620da65284c92
iXZed026 <danyal.titanka24@gmail.com>
2024-08-28 16:17:29 +0330

    add suggestions for search

--- src/components/Header/Search.js
+++ src/components/Header/Search.js
@@ -1,30 +1,58 @@
-import React ,{useRef}from 'react';
+import React, { useRef, useState } from 'react';
 import "./Search.css";
 
+let scrollRef = useRef(null);
 const Search = () => {
-
+    
     const searchAniRef = useRef();
+    const searchSuggestions = ["mobile", "laptop", "موبایل", "لپ تاپ"];
+    const [suggestItems, setSuggestItems] = useState([])
+    const [searchSuggestionsFlag, setSearchSuggestionsFlag] = useState(false);
 
-    const searchOpration = (e)=>{
-            searchAniRef.current.style.display = "block";
-            searchAniRef.current.className = "search-animation";
-            e.target.style.backgroundColor = "#c3c3c3";
+    const searchOpration = (e) => {
+        searchAniRef.current.style.display = "block";
+        searchAniRef.current.className = "search-animation";
+        e.target.style.backgroundColor = "#c3c3c3";
+        setSearchSuggestionsFlag(true);
+    }
+
+    const searchBlur = (e) => {
+        e.target.style.backgroundColor = "#D6D6DC"
+        searchAniRef.current.style.display = "none";
+        setSearchSuggestionsFlag(false);
+
+    }
+
+
+    const suggestionHandler = (e) => {
+
+        const value = e.target.value;
+
+        const fillteringSuggestion = searchSuggestions.filter(suggest => {
+            return suggest.toLowerCase().startsWith(value.toLowerCase());
+        });
+        if (value) {
+            setSuggestItems(fillteringSuggestion);
+        } else {
+            setSuggestItems([]);
         }
-        
-        const searchBlur = (e)=>{
-            e.target.style.backgroundColor = "#D6D6DC"
-            searchAniRef.current.style.display = "none";
+
     }
 
     return (
         <>
-            <input type="search" placeholder=' 🔍 جستو جو' onBlur={searchBlur} onFocus={searchOpration}/>
+            <input type="search" placeholder=' 🔍 جستو جو' onBlur={searchBlur} onFocus={searchOpration} onKeyUp={suggestionHandler} />
             <div ref={searchAniRef} ></div>
-            <ul id="suggestion-search">
-                
-            </ul>
+            {
+                searchSuggestionsFlag && (
+                    <ul id="search-suggestions">
+                        {suggestItems.map((item, key) => <li id="search-suggestions-item">{item}</li>)}
+                    </ul>
+                )
+            }
         </>
     )
 }
 
-export default Search
+export {scrollRef}
+export default {Search};
