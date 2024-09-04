e9f8057d7d860d88d54e93479b4917ce08c8ac27
iXZed026 <danyal.titanka24@gmail.com>
2024-09-04 13:01:59 +0330

    create guidelines component

---
+++ src/components/Guidelines/Guidelines.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Guidelines.css";
+
+const Guidelines = () => {
+  return (
+    <div className="guidelines">
+        <div className="guidelines-container">
+            <div className="guidelines-flex">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Guidelines
