df96f974a0096e27e5be8381f072fffeea80a350
iXZed026 <danyal.titanka24@gmail.com>
2024-09-04 16:46:54 +0330

    create Address component

---
+++ src/components/Contact/Address.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Address.css";
+
+const Address = () => {
+  return (
+    <div className="addres">
+        <div className="address-header">
+            <div className="address-body">
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Address
