711ccbeb51b03acef0c6cbe30613200080179c86
iXZed026 <danyal.titanka24@gmail.com>
2024-09-04 23:20:25 +0330

    compelte contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import "./Contact.css";
 import Map from './Map';
+import Address from './Address';
 
 const Contact = () => {
     return (
         <div className="contact">
             <div className="contact-container">
                 <div className="contact-flex">
+                    <Address />
                     <Map />
                 </div>
             </div>
