f40ef78fea8419d5ddcca306f2324a613b302da0
iXZed026 <danyal.titanka24@gmail.com>
2024-09-04 16:05:06 +0330

    create map component

---
+++ src/components/Contact/Map.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Map.css";
+
+const Map = () => {
+  return (
+    <div className="map">
+        <div className="map-container">
+            <div className="map-flex">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Map
